1abf19d8ef932fb86052c419559c3bd52323756a
libowen424 <1183936459@qq.com>
2020-02-18 17:29:38 +0800

    第一次作业

---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.My_Conbobox();
+        }
+
+        public void My_Conbobox()
+        {
+            comboBox1.Items.Add("+");
+            comboBox1.Items.Add("-");
+            comboBox1.Items.Add("*");
+            comboBox1.Items.Add("/");
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedItem.ToString()) //获取选择的内容
+            {
+
+                case "+": label3.Text = "+"; break;
+
+                case "-": label3.Text = "-"; break;
+
+                case "*": label3.Text = "*"; break;
+
+                case "/": label3.Text = "/";  break;
+
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try {
+                double firstNumber, secondNumber;
+                char sign;
+                firstNumber = Double.Parse(textBox1.Text);
+                secondNumber = Double.Parse(textBox2.Text);
+                sign = Char.Parse(label3.Text);
+                switch (sign)
+                {
+                    case '+':
+                        label4.Text = (firstNumber + secondNumber).ToString();
+                        break;
+                    case '-':
+                        label4.Text = (firstNumber - secondNumber).ToString();
+                        break;
+                    case '*':
+                        label4.Text = (firstNumber * secondNumber).ToString();
+                        break;
+                    case '/':
+                        label4.Text = (firstNumber / secondNumber).ToString();
+                        break;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            label3.Text = "";
+            label4.Text = "";
+           
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                double firstNumber, secondNumber;
+                char sign;
+                Console.WriteLine("请输入第一个数");
+                firstNumber = Double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+                Console.WriteLine("请输入第二个数");
+                secondNumber = Double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+                Console.WriteLine("请输入符号");
+                sign = Char.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+                switch (sign)
+                {
+                    case '+':
+                        Console.Write("答案是 ");
+                        Console.WriteLine(firstNumber + secondNumber);
+                        break;
+                    case '-':
+                        Console.Write("答案是 ");
+                        Console.WriteLine(firstNumber - secondNumber);
+                        break;
+                    case '*':
+                        Console.Write("答案是 ");
+                        Console.WriteLine(firstNumber * secondNumber);
+                        break;
+                    case '/':
+                        if(secondNumber==0)
+                        {
+                            Console.Write("被除数不能为0 ");
+                        }
+                        Console.Write("答案是 ");
+                        Console.WriteLine(firstNumber / secondNumber);
+                        break;
+                    default:
+                        Console.WriteLine("请输入一个符号！！！");
+                        break;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
