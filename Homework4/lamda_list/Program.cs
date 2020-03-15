947364a074a15fc209d1285b219be90f7cba3611
libowen424 <1183936459@qq.com>
2020-03-15 09:34:59 +0800

    第四次

---
+++ Homework4/lamda_list/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lamda_list
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if(tail==null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void Foreach(Action<T> act)
+        {
+            Node<T> temp;
+            temp = head;
+            if (temp == null)
+                return;
+            act(temp.Data);
+                while(temp!=tail)
+            {
+                temp = temp.Next;
+                act(temp.Data);
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            GenericList<int> intlist = new GenericList<int>();
+            for(int x=0;x<10;x++)
+            {
+                intlist.Add(x);
+            }
+
+            Action<int> printout = delegate (int item)
+             {
+                 Console.WriteLine(item);
+             };
+            intlist.Foreach(printout);
+            intlist.Foreach(x => sum += x);
+            Console.WriteLine(sum);
+        }
+    }
+}
